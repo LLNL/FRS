34ff51e5a2a14636a1ceee961f83f0f9dcaae2aa
Lee, John <lee1048@llnl.gov>
2019-08-10 18:47:52 -0700

    Camera viewer added

--- FRS/MainViewController.swift
+++ FRS/MainViewController.swift
@@ -7,24 +7,102 @@
 //
 
 import UIKit
+import Foundation
+import AVFoundation
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCapturePhotoCaptureDelegate {
+    
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var btnRecognize: UIButton!
+    
+    var infoLinksMap: [Int:String] = [1000:""]
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        self.captureSession.stopRunning()
+        setupSession()
     }
-    */
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupSession()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func onRecognizeClicked(_ sender: Any) {
+        
+    }
+    
+    //Setup camera session
+    func setupSession(){
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+            else {
+                print("Unable to access back camera!")
+                return
+        }
+        
+        do {
+            if(backCamera.isFocusModeSupported(.continuousAutoFocus)){
+                try! backCamera.lockForConfiguration()
+                backCamera.focusMode = .continuousAutoFocus
+                backCamera.unlockForConfiguration()
+            }
+            let input = try AVCaptureDeviceInput(device: backCamera)
 
+            stillImageOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+        }
+        catch let error  {
+            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
+        }
+    }
+    
+    //Setup live preview, handle rotations
+    func setupLivePreview() {
+        previewView.layer.sublayers?.removeAll()
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        videoPreviewLayer.videoGravity = .resizeAspect
+        if UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft {
+            videoPreviewLayer.connection?.videoOrientation = .landscapeRight
+        } else if UIDevice.current.orientation == UIDeviceOrientation.landscapeRight {
+            videoPreviewLayer.connection?.videoOrientation = .landscapeLeft
+        } else if UIDevice.current.orientation == UIDeviceOrientation.portrait {
+            videoPreviewLayer.connection?.videoOrientation = .portrait
+        } else if UIDevice.current.orientation == UIDeviceOrientation.portraitUpsideDown{
+            videoPreviewLayer.connection?.videoOrientation = .portraitUpsideDown
+        }
+        previewView.layer.addSublayer(videoPreviewLayer)
+        
+        DispatchQueue.global(qos: .userInitiated).async { //[weak self] in
+            self.captureSession.startRunning()
+
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.previewView.bounds
+            }
+        }
+    }
 }
